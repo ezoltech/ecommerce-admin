69d1d8520d7a5025c659298a21e0aabe4b7dfe15
ezoltech <ezramamo13@gmail.com>
2023-09-01 14:51:20 +0300

    creating billboard page

---
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import React from 'react'
+
+export const BillboardClient = () => {
+  return (
+    <>
+    <div className="flex items-center justify-between ">
+        Billboard page goes here
+    </div>
+    </>
+  )
+}
